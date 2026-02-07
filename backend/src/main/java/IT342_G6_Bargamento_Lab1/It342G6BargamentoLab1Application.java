ef0261f2d64f2b360e559034b257c89ddc800526
LAB13_USER ON WS15 <L13Y17W15@LabHCI.net>
2026-02-07 17:18:28 +0800

    Initial Commmit

---
+++ backend/src/main/java/IT342_G6_Bargamento_Lab1/It342G6BargamentoLab1Application.java
@@ -0,0 +1,13 @@
+package IT342_G6_Bargamento_Lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342G6BargamentoLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342G6BargamentoLab1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/IT342_G6_Bargamento_Lab1/It342G6BargamentoLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package IT342_G6_Bargamento_Lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342G6BargamentoLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
